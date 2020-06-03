a3c3eb423579830402fc06353833c0932684effe
narsimha-na <activena1@gmail.com>
2020-06-03 23:24:01 +0530

    Initial commit

---
+++ app/src/main/java/com/maverick/nacode/ui/authentication/Authentication.kt
@@ -0,0 +1,12 @@
+package com.maverick.nacode.ui.authentication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.maverick.nacode.R
+
+class Authentication : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authentication)
+    }
+}
